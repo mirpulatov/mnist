2c5fcaeb1e11bc25e99a11846e973932ab0575e1
Mir <mir@MacBook-Pro-Mir.local>
2018-10-15 13:04:40 +0500

    Added input data and weights after 15 epoch

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import NeuralNetwork as Network
 import pickle
+import gzip
 
-# Loading data from pkl file
-with open("mnist.pkl", "br") as fh:
-    data = pickle.load(fh)
+f = gzip.open('mnist.pkl.gz', 'rb')
+data = pickle.load(f)
 train_images = data[0]
 test_images = data[1]
 train_labels = data[2]
 test_labels = data[3]
 train_label = data[4]
+f.close()
 
-NNet = Network.NeuralNetwork(input_layer=784, output_layer=10, hidden_layer=300, learning_rate=0.05)
+NNet = Network.NeuralNetwork(input_layer=784, output_layer=10, hidden_layer=300, learning_rate=0.03)
 
-epoch = 10
+epoch = 15
 
 for i in range(1, epoch + 1):
     print("Epoch {0}:".format(i))
@@ -24,3 +25,4 @@ for i in range(1, epoch + 1):
     print("Accuracy on training set: ", corrects / (corrects + wrongs))
     corrects, wrongs = NNet.evaluate(test_images, test_labels)
     print("Accuracy on test set: ", corrects / (corrects + wrongs))
+
